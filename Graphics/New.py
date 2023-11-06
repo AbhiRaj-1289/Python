2245234c0bcf456e1a06fa43b0573bb09ac06e30
Abhinav Raj <abhiraj61002@gmail.com>
2023-11-06 10:43:37 +0530

    new

--- Graphics/New.py
+++ Graphics/New.py
@@ -1,10 +1,30 @@
-from turtle import *
 import turtle
-from random import random
 
- 
-for steps in range(100):
-    for c in ('blue', 'red', 'green'):
-        color(c)
-        forward(steps)
-        right(30)
+# Create a turtle screen
+screen = turtle.Screen()
+screen.bgcolor("white")
+
+# Create a turtle object
+star_turtle = turtle.Turtle()
+star_turtle.shape("turtle")
+star_turtle.speed(5)  # Set the drawing speed (1 is slowest, 0 is fastest)
+
+# Function to draw a star
+def draw_star(size):
+    for _ in range(5):
+        star_turtle.forward(size)
+        star_turtle.right(144)
+
+# Move the turtle to a starting position
+star_turtle.penup()
+star_turtle.goto(0, -100)  # Adjust the starting position as needed
+star_turtle.pendown()
+
+# Set the star size
+star_size = 200
+
+# Draw the star
+draw_star(star_size)
+
+# Close the turtle graphics window on click
+screen.exitonclick()
