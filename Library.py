c1b77810adddfbe86888ddd771926a44c11747b6
Abhinav Raj <abhiraj61002@gmail.com>
2024-01-19 20:32:25 +0530

    new

---
+++ Library.py
@@ -0,0 +1,8 @@
+elements = 0
+number_list = []
+print("Enter Elements : ")
+for i in range(0,11):
+    elements  = int(input())
+    number_list.append(elements)
+for values in number_list:
+    print(values)
