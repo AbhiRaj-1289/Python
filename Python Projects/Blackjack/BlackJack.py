cc48a58d17948c3fe4a1576b1e3a521599bded02
Abhinav Raj <abhiraj61002@gmail.com>
2024-10-09 12:10:10 +0530

    modified

--- Python Projects/Blackjack/BlackJack.py
+++ Python Projects/Blackjack/BlackJack.py
@@ -8,7 +8,7 @@
 ## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
-# The computer is the dealer.
+## The computer is the dealer.
 
 ####################################################################
 
